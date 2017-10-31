2fdf1f246cf81ed5e18012e47801c76d2827bf41
nehuen <onamonzon.32@gmail.com>
2017-10-30 23:23:20 -0300

    iniciando

---
+++ tp-dias-de-la-semana.js
@@ -0,0 +1,10 @@
+var diasDeLaSemana = prompt("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo");
+    alert(diasDeLaSemana)
+
+    if (diasDeLaSemana == "sabado"){
+    alert("es fin de semana");
+} else if (diasDeLaSemana == "domingo"){
+    alert("es fin de semana");
+}else {
+    alert("es dia habil")
+}  
